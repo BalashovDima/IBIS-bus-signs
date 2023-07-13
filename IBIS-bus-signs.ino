4a03f47b6f2a858952ceaf53706c6af4c6ec8b27
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-07-13 11:25:49 +0300

    Add LCD display
    Add setup code to control LCD display. Uses LiquidCrystal 1.0.7 library

--- IBIS-bus-signs.ino
+++ IBIS-bus-signs.ino
@@ -1,4 +1,19 @@
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+#define rs 8
+#define en 9
+#define d4 4 
+#define d5 5
+#define d6 6
+#define d7 7
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define LCD_columns 16
+#define LCD_rows 2
+
 void setup() {
+    lcd.begin(LCD_columns, LCD_rows); // set up the LCD's number of columns and rows
 
 }
 
