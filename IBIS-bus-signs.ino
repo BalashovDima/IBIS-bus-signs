8ce3c97fa14ce96781bb5b1137bf71aee2a3356e
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-07-13 17:50:42 +0300

    Add RTC module
    Add code to work with RTC module. Uses RTClib 2.1.1 library

--- IBIS-bus-signs.ino
+++ IBIS-bus-signs.ino
@@ -1,5 +1,9 @@
 #include <LiquidCrystal.h>
+#include "RTClib.h"
 
+
+#define LCD_columns 16
+#define LCD_rows 2
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 #define rs 8
@@ -9,14 +13,25 @@
 #define d6 6
 #define d7 7
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-#define LCD_columns 16
-#define LCD_rows 2
+
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 void setup() {
     lcd.begin(LCD_columns, LCD_rows); // set up the LCD's number of columns and rows
 
+    if (!rtc.begin()) {
+        // code to do if failed to connect to rtc module
+        // Serial.println("Couldn't find RTC");
+    }
+
+    // rtc.adjust(DateTime()); // set rtc module time
+
 }
 
 void loop() {
+    DateTime now = rtc.now();
 
 }
