1b0336870dac3ba1eca0f43fbe29806da024aa77
BalashovDima <97038388+BalashovDima@users.noreply.github.com>
2023-07-14 23:46:14 +0300

    Main menu responsive to buttons. State changes
    When in main menu, clicking buttons does certain actions (changes option, selects option and goes to it's setting(not implemented yet))
    
    Replaced menu_state with state - more general

--- IBIS-bus-signs.ino
+++ IBIS-bus-signs.ino
@@ -29,11 +29,14 @@ EncButton<EB_TICK, VIRT_BTN> down_btn;
 EncButton<EB_TICK, VIRT_BTN> up_btn;
 EncButton<EB_TICK, VIRT_BTN> right_btn;
 
-// each menu state marks certain 'menu state'. for each state (each number) the text of the menu is different
+// each state marks certain 'state'. for each state (each number) the text of the menu is different 
 // the codes are as follows:
 // 0 -- home | stand-by mode. not in menu. date, time and line are shown
-// 1 -- main menu | 
-uint8_t menu_state = 0;
+// 1 -- main menu | pointed at 'Line' option 1st row . 'Other text' 2nd row
+// 2 -- main menu | 'Line' option 1st row . pointed at 'Other text' 2nd row
+// 3 -- main menu | pointed at 'Time settings' option 1st row. 2nd row empty
+
+uint16_t state = 0;
 String date_and_time;
 String current_sign_text = "Line 1";
 
@@ -42,7 +45,7 @@ uint8_t second = 0;
 void updateMenu(bool state_changed = false) {
     if(state_changed) lcd.clear();
 
-    switch (menu_state) {
+    switch (state) {
         case 0: 
             lcd.home();
             lcd.print(date_and_time);
@@ -50,7 +53,21 @@ void updateMenu(bool state_changed = false) {
             lcd.print(current_sign_text);
             break;
         case 1:
-
+            lcd.setCursor(0,0);
+            lcd.print("> Line");
+            lcd.setCursor(0,1);
+            lcd.print("  Other text");
+            break;
+        case 2:
+            lcd.setCursor(0,0);
+            lcd.print("  Line");
+            lcd.setCursor(0,1);
+            lcd.print("> Other text");
+            break;
+        case 3:
+            lcd.setCursor(0,0);
+            lcd.print("> Time settings");
+            break;
         default:
             break;
     }
@@ -90,9 +107,37 @@ void loop() {
     up_btn.tick(keys.status(3));
     right_btn.tick(keys.status(4));
 
+    if(state == 0 && select_btn.click()) {
+        state = 1; // enters menu
+        updateMenu(true);
+    } else if(state <= 3) {
+        if(select_btn.click()) {
+            switch(state) {
+                case 1:
+                    // state = ;
+                    // updateMenu(true);
+                    break;
+                case 2:
+                    // state = ;
+                    // updateMenu(true);
+                    break;
+                case 3:
+                    // state = ;
+                    // updateMenu(true);
+                    break;
+            }
+        } else if(down_btn.click()) {
+            state + 1 > 3 ? state = 1 : state++;
+            updateMenu(true);
+        } else if(up_btn.click()) {
+            state - 1 < 1 ? state = 3 : state--;
+            updateMenu(true);
+        }
+    }
+
     DateTime now = rtc.now();
 
-    if(second != now.second()) {
+    if(state == 0 && second != now.second()) {
         char date_time[] = "DD/MM  hh:mm:ss";
         now.toString(date_time);
         date_and_time = date_time;
